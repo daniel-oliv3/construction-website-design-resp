a528888fdf96f8af83ad0418400f8eefa9bb1e39
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-18 17:55:05 -0300

    Javascript update

--- js/script.js
+++ js/script.js
@@ -5,14 +5,20 @@ let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
 };
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
 };
 
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 };
 
 document.querySelector('#info-btn').onclick = () => {
@@ -28,4 +34,5 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     loginForm.classList.remove('active');
+    contactInfo.classList.remove('active');
 }
