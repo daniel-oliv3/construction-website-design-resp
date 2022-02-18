de2d73438a3c3ed1fe320075971c11033a5485d7
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-18 16:12:28 -0300

    LoginForm

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let navbar = document.querySelector('.header .navbar');
 let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
@@ -9,7 +10,12 @@ document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 };
 
+document.querySelector('#login-form').onclick = () => {
+    loginForm.classList.toggle('active');
+};
+
 window.onscroll = () => {
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
