e483c4e3f450e5dd4643ce7e2ddf7eff55e00ebf
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-18 17:39:47 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let navbar = document.querySelector('.header .navbar');
 let searchForm = document.querySelector('.header .search-form');
 let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
@@ -13,6 +14,15 @@ document.querySelector('#search-btn').onclick = () => {
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
 };
+
+document.querySelector('#info-btn').onclick = () => {
+    contactInfo.classList.add('active');
+};
+
+document.querySelector('#close-contact-info').onclick = () => {
+    contactInfo.classList.remove('active');
+};
+
 
 window.onscroll = () => {
     navbar.classList.remove('active');
